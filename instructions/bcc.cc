8e44a16c103b3f639603049d87691657f41288cc
SHIMOMURA Sho <graueule@gmail.com>
2021-01-09 00:23:17 +0900

    bugfix

--- instructions/bcc.cc
+++ instructions/bcc.cc
@@ -3,7 +3,9 @@
 int h8instructions::bcc::bra_8(H8300H* h8)
 {
     int8_t disp = h8->fetch_instruction_byte(1);
-    h8->pc += 2 + disp;
+    h8->pc += 2;
+    h8->pc += disp;
+
     return 0;
 }
 
@@ -14,14 +16,16 @@ int h8instructions::bcc::bra_16(H8300H* h8)
     displacement[0] = h8->fetch_instruction_byte(3);
     int16_t disp = *(int16_t*)displacement;
 
-    h8->pc += 4 + disp;
+    h8->pc += 4;
+    h8->pc += disp;
+
     return 0;
 }
 
 int h8instructions::bcc::bls_8(H8300H* h8)
 {
     int8_t disp = h8->fetch_instruction_byte(1);
-    h8->pc += 2 + disp;
+    h8->pc += 2;
 
     if (h8->ccr.c() || h8->ccr.z()) {
         h8->pc += disp;
