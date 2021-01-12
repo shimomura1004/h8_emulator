3d6b15af00facd85b1f9ec052611d07ae59a3b91
SHIMOMURA Sho <graueule@gmail.com>
2021-01-12 23:12:03 +0900

    add sender command

---
+++ sender.c
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/select.h>
+
+#define R (0)
+#define W (1)
+
+#define XMODEM_SOH 0x01
+#define XMODEM_STX 0x02
+#define XMODEM_EOT 0x04
+#define XMODEM_ACK 0x06
+#define XMODEM_NAK 0x15
+#define XMODEM_CAN 0x18
+#define XMODEM_EOF 0x1a
+
+#define XMODEM_BLOCK_SIZE 128
+
+static int fd_r, fd_w;
+
+int popen2(char* command, int* fd_r, int* fd_w)
+{
+    int pipe_c2p[2],pipe_p2c[2];
+    int pid;
+
+    if (pipe(pipe_c2p) < 0) {
+        perror("popen2");
+        return(-1);
+    }
+    if (pipe(pipe_p2c) < 0) {
+        perror("popen2");
+        close(pipe_c2p[R]);
+        close(pipe_c2p[W]);
+        return -1;
+    }
+
+    if ((pid=fork()) < 0){
+        perror("popen2");
+        close(pipe_c2p[R]);
+        close(pipe_c2p[W]);
+        close(pipe_p2c[R]);
+        close(pipe_p2c[W]);
+        return -1;
+    }
+    if (pid == 0){
+        // 子プロセス
+        close(pipe_p2c[W]);
+        close(pipe_c2p[R]);
+        dup2(pipe_p2c[R], 0);
+        dup2(pipe_c2p[W], 1);
+        close(pipe_p2c[R]);
+        close(pipe_c2p[W]);
+
+        // h8300h を子プロセスとして起動
+        // if(execlp("./h8300h", "./h8300h", "1", NULL) < 0){
+        if(execlp("./h8300h", "./h8300h", NULL) < 0){
+            perror("popen2");
+            close(pipe_p2c[R]);
+            close(pipe_c2p[W]);
+            exit(1);
+        }
+    }
+
+    close(pipe_p2c[R]);
+    close(pipe_c2p[W]);
+    *fd_w=pipe_p2c[W];
+    *fd_r=pipe_c2p[R];
+
+    return(pid);
+}
+
+int check_send(char* buf)
+{
+    if (strncmp(buf, "sendos", 6) != 0)
+        return 0;
+
+    printf("Sending OS binary.\n");
+
+    int n;
+
+    // // read NAK
+    // int size = read(fd_r, &n, 1);
+    // if (n != 0x15) {
+    //     printf("not nak.\n");
+    //     buf[0] = '\n';
+    //     buf[1] = '\0';
+    //     return 0;
+    // }
+
+    // // send SOH
+    // char c = XMODEM_SOH;
+    // write(fd_w, &c, 1);
+
+    // prepare file descriptor
+    FILE *fp = fopen("main.cc", "rb");
+    if (!fp) {
+        printf("file not found.\n");
+        buf[0] = '\n';
+        buf[1] = '\0';
+        return 0;
+    }
+
+    // send blocks
+    unsigned char count = 1;
+    unsigned char os_buf[128];
+    printf("Sending blocks");
+    fflush(stdout);
+    while (1) {
+        // printf("Sending block %d\n", count);
+        putchar('.');
+        fflush(stdout);
+        // read ACK or NAK
+        // とりあえずエラー(NAK)でも無視する
+        read(fd_r, &n, 1);
+
+        // send SOH
+        char c = XMODEM_SOH;
+        write(fd_w, &c, 1);
+
+        write(fd_w, &count, 1);
+        char invert_count = ~count;
+        write(fd_w, &invert_count, 1);
+
+        memset(os_buf, EOF, 128);
+        int size = fread(os_buf, sizeof(char), 128, fp);
+        write(fd_w, os_buf, 128);
+
+        unsigned char check_sum = 0;
+        for (int i = 0; i < 128; i++) {
+            check_sum += os_buf[i];
+        }
+
+        write(fd_w, &check_sum, 1);
+
+        if (size != 128) {
+            // 最後に EOT を送り、ACK を待つ
+            char c = XMODEM_EOT;
+            write(fd_w, &c, 1);
+
+            printf("done.\n");
+
+            read(fd_r, &n, 1);
+            printf("ACK is %02x\n", n);
+            buf[0] = '\n';
+            buf[1] = '\0';
+            break;
+        }
+
+        count++;
+    }
+
+    fclose(fp);
+
+    return 1;
+}
+
+int main(int argc, char* argv[])
+{
+    char buf[256];
+
+    fd_set fdset;
+	struct timeval timeout;
+	timeout.tv_sec = 0; 
+	timeout.tv_usec = 0;
+
+    popen2(NULL, &fd_r, &fd_w);
+
+    while (1) {
+        FD_ZERO(&fdset);
+        FD_SET(0, &fdset);
+        FD_SET(fd_r, &fdset);
+
+        int ret = select(fd_r + 1, &fdset , NULL , NULL , &timeout);
+
+        if (ret == 1) {
+            // h8 からの出力
+            if (FD_ISSET(fd_r, &fdset)) {
+                int size = read(fd_r, buf, 255);
+                buf[size] = '\0';
+                printf("%s", buf);
+                fflush(stdout);
+            }
+            // ユーザの入力
+            if (FD_ISSET(0, &fdset)) {
+                int size = read(0, buf, 256);
+                buf[size] = '\0';
+                if (size >= 0) {
+                    buf[size] = '\0';
+                    if (!check_send(buf)) {
+                        write(fd_w, buf, size);
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
