d41e31b07fe386e29d837521136c083f2057c71e
SHIMOMURA Sho <graueule@gmail.com>
2021-01-15 10:07:58 +0900

    clear interrupt after handling

--- instructions/rte.cc
+++ instructions/rte.cc
@@ -3,5 +3,10 @@
 int h8instructions::rte::rte(H8300H *h8)
 {
     h8->restore_pc_and_ccr_from_stack();
+
+    // 割り込みから戻ったら割り込み要因をクリアする
+    interrupt_t type = h8->interrupt_controller.getInterruptType();
+    h8->interrupt_controller.clear(type);
+
     return 0;
 }
