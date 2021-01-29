bd0bfd29b3572d7a0a12e2fa27139c654cc546fe
SHIMOMURA Sho <graueule@gmail.com>
2021-01-29 17:30:55 +0900

    bugfix

--- instructions/trapa.cc
+++ instructions/trapa.cc
@@ -10,9 +10,13 @@ static interrupt_t interrupt_table[4] = {
 int h8instructions::trapa::trapa(H8300H *h8)
 {
     uint8_t b1 = h8->fetch_instruction_byte(1);
-    uint8_t imm = (b1 & 0x0f00) >> 4;
+    uint8_t imm = (b1 & 0x30) >> 4;
 
+    // 割り込みを発生させる
     h8->interrupt_controller.set(interrupt_table[imm]);
+
+    // CCR.I ビットは割り込み処理部でセットされる
+
     h8->pc += 2;
 
     return 0;
