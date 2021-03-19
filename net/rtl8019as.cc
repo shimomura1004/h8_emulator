52ac8c680271650e0e8755e9ffc36de404b2f354
SHIMOMURA Sho <graueule@gmail.com>
2021-03-19 18:53:56 +0900

    add registers and table

--- net/rtl8019as.cc
+++ net/rtl8019as.cc
@@ -1,29 +1,87 @@
 #include "rtl8019as.h"
 
+uint8_t RTL8019AS::getPage()
+{
+    return (this->CR & 0b11000000) >> 6;
+}
+
+uint8_t* RTL8019AS::getRegister(uint8_t index, uint8_t page, bool read_mode)
+{
+    return register_table[page][read_mode ? 0 : 1][index];
+}
+
+// todo: レジスタの初期値の設定
 RTL8019AS::RTL8019AS()
+    : CR(0x00)
+    , CLDA0(0x00)
+    , CLDA1(0x00)
+    , BNRY(0x00)
+    , TSR(0x00)
+    , NCR(0x00)
+    , FIFO(0x00)
+    , ISR(0xff)
+    , CRDA0(0x00)
+    , CRDA1(0x00)
+    , CHIP8019ID0(0x00)
+    , CHIP8019ID1(0x00)
+    , RSR(0x00)
+    , CNTR0(0x00)
+    , CNTR1(0x00)
+    , CNTR2(0x00)
+    , PSTART(0x00)
+    , PSTOP(0x00)
+    , TPSR(0x00)
+    , TBCR0(0x00)
+    , TBCR1(0x00)
+    , RSAR0(0x00)
+    , RSAR1(0x00)
+    , RBCR0(0x00)
+    , RBCR1(0x00)
+    , RCR(0x00)
+    , TCR(0x00)
+    , DCR(0x00)
+    , IMR(0x00)
+    , PAR0(0x00)
+    , PAR1(0x00)
+    , PAR2(0x00)
+    , PAR3(0x00)
+    , PAR4(0x00)
+    , PAR5(0x00)
+    , CURR(0x00)
+    , MAR0(0x00)
+    , MAR1(0x00)
+    , MAR2(0x00)
+    , MAR3(0x00)
+    , MAR4(0x00)
+    , MAR5(0x00)
+    , MAR6(0x00)
+    , MAR7(0x00)
+    , CR9346CR(0x00)
+    , BPAGE(0x00)
+    , CONFIG0(0x00)
+    , CONFIG1(0x00)
+    , CONFIG2(0x00)
+    , CONFIG3(0x00)
+    , CSNSAV(0x00)
+    , INTR(0x00)
+    , CONFIG4(0x00)
+    , TEST(0x00)
+    , HLTCLK(0x00)
+    , FMWP(0x00)
 {}
 
 uint8_t RTL8019AS::read8(uint32_t address)
 {
-}
+    if (address > 0x1f) {
+        fprintf(stderr, "Error: Invalid access to RTL8019AS register (0x%x)", address);
+        return 0;
+    }
 
-uint16_t RTL8019AS::read16(uint32_t address)
-{
-}
-
-uint32_t RTL8019AS::read32(uint32_t address)
-{
+    uint8_t page = getPage();
+    return *getRegister(address, page, true);
 }
 
 void RTL8019AS::write8(uint32_t address, uint8_t value)
-{
-}
-
-void RTL8019AS::write16(uint32_t address, uint16_t value)
-{
-}
-
-void RTL8019AS::write32(uint32_t address, uint32_t value)
 {
 }
 
--- net/rtl8019as.h
+++ net/rtl8019as.h
@@ -5,16 +5,128 @@
 #include <cstdint>
 
 class RTL8019AS {
+    // page0, read
+    uint8_t CR;
+    uint8_t CLDA0;
+    uint8_t CLDA1;
+    uint8_t BNRY;
+    uint8_t TSR;
+    uint8_t NCR;
+    uint8_t FIFO;
+    uint8_t ISR;
+    uint8_t CRDA0;
+    uint8_t CRDA1;
+    uint8_t CHIP8019ID0;
+    uint8_t CHIP8019ID1;
+    uint8_t RSR;
+    uint8_t CNTR0;
+    uint8_t CNTR1;
+    uint8_t CNTR2;
+
+    // page0, write
+    uint8_t PSTART;
+    uint8_t PSTOP;
+    uint8_t TPSR;
+    uint8_t TBCR0;
+    uint8_t TBCR1;
+    uint8_t RSAR0;
+    uint8_t RSAR1;
+    uint8_t RBCR0;
+    uint8_t RBCR1;
+    uint8_t RCR;
+    uint8_t TCR;
+    uint8_t DCR;
+    uint8_t IMR;
+
+    // page1, read/write
+    uint8_t PAR0;
+    uint8_t PAR1;
+    uint8_t PAR2;
+    uint8_t PAR3;
+    uint8_t PAR4;
+    uint8_t PAR5;
+    uint8_t CURR;
+    uint8_t MAR0;
+    uint8_t MAR1;
+    uint8_t MAR2;
+    uint8_t MAR3;
+    uint8_t MAR4;
+    uint8_t MAR5;
+    uint8_t MAR6;
+    uint8_t MAR7;
+
+    // page2
+    // page3, read
+    uint8_t CR9346CR;
+    uint8_t BPAGE;
+    uint8_t CONFIG0;
+    uint8_t CONFIG1;
+    uint8_t CONFIG2;
+    uint8_t CONFIG3;
+    uint8_t CSNSAV;
+    uint8_t INTR;
+    uint8_t CONFIG4;
+
+    // page3, write
+    uint8_t TEST;
+    uint8_t HLTCLK;
+    uint8_t FMWP;
+
+    uint8_t* const register_table[4][2][16] = {
+        {
+            {
+                &CR, &CLDA0, &CLDA1, &BNRY, &TSR, &NCR, &FIFO, &ISR,
+                &CRDA0, &CRDA1, &CHIP8019ID0, &CHIP8019ID1, &RSR, &CNTR0, &CNTR1, &CNTR2,
+            },
+            {
+                &CR, &PSTART, &PSTOP, &TPSR, &BNRY, &TBCR0, &TBCR1, &ISR,
+                &RSAR0, &RSAR1, &RBCR0, &RBCR1, &RCR, &TCR, &DCR, &IMR, 
+            }
+        },
+        {
+            {
+                &CR, &PAR0, &PAR1, &PAR2, &PAR3, &PAR4, &PAR5, &CURR,
+                &MAR0, &MAR1, &MAR2, &MAR3, &MAR4, &MAR5, &MAR6, &MAR7,
+            },
+            {
+                &CR, &PAR0, &PAR1, &PAR2, &PAR3, &PAR4, &PAR5, &CURR,
+                &MAR0, &MAR1, &MAR2, &MAR3, &MAR4, &MAR5, &MAR6, &MAR7,
+            }
+        },
+        {
+            {
+                &CR, &PSTART, &PSTOP, nullptr, &TPSR, nullptr, nullptr, nullptr,
+                nullptr, nullptr, nullptr, nullptr, &RCR, &TCR, &DCR, &IMR,
+            },
+            {
+                &CR, &PSTART, &PSTOP, nullptr, &TPSR, nullptr, nullptr, nullptr,
+                nullptr, nullptr, nullptr, nullptr, &RCR, &TCR, &DCR, &IMR,
+            },
+        },
+        {
+            {
+                &CR, &CR9346CR, &BPAGE, &CONFIG0, &CONFIG1, &CONFIG2, &CONFIG3, nullptr,
+                &CSNSAV, nullptr, nullptr, &INTR, nullptr, &CONFIG4, nullptr, nullptr,
+            },
+            {
+                &CR, &CR9346CR, &BPAGE, nullptr, &CONFIG1, &CONFIG2, &CONFIG3, &TEST,
+                nullptr, &HLTCLK, nullptr, nullptr, &FMWP,  nullptr, nullptr, nullptr,
+            }
+        }
+    };
+
+    uint8_t getPage();
+    uint8_t* getRegister(uint8_t index, uint8_t page, bool read_mode);
 
 public:
     RTL8019AS();
 
     uint8_t read8(uint32_t address);
-    uint16_t read16(uint32_t address);
-    uint32_t read32(uint32_t address);
+    // uint16_t read16(uint32_t address);
+    // uint32_t read32(uint32_t address);
     void write8(uint32_t address, uint8_t value);
-    void write16(uint32_t address, uint16_t value);
-    void write32(uint32_t address, uint32_t value);
+    // void write16(uint32_t address, uint16_t value);
+    // void write32(uint32_t address, uint32_t value);
 
     void dump(FILE* fp);
 
