4bff0c45ff4379f775917aff1fbe5f5e8a32acb0
SHIMOMURA Sho <graueule@gmail.com>
2021-05-01 13:45:39 +0900

    gtest

--- test/example.cc
+++ test/example.cc
@@ -1,13 +1,7 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-TEST(sumA1, normal)
-{
-	ASSERT_EQ(3, 1 + 2);
-}
-
-int main(int argc, char **argv)
-{
-	::testing::InitGoogleMock(&argc, argv);
-	return RUN_ALL_TESTS();
-}
+// TEST(sumA1, normal)
+// {
+// 	ASSERT_EQ(3, 1 + 2);
+// }
---
+++ test/instructions/mov_immediate_test.cc
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "instructions/mov_immediate.h"
+
+TEST(immediate_b_parse, normal)
+{
+     EXPECT_EQ(1, 1);
+}
