d374096b2930469cf0825f93c3389d87ba9dfcf9
SHIMOMURA Sho <graueule@gmail.com>
2021-05-11 21:54:48 +0900

    add test for mov register indirect with displacement 16 b

--- test/instructions/mov_register_indirect_with_displacement_test.cc
+++ test/instructions/mov_register_indirect_with_displacement_test.cc
@@ -5,7 +5,7 @@
 // 命令の実行のテストと、命令のパースのテストは違う
 // ここで確認するのはパーサの選択やパースの実行結果ではなく、命令の実行結果
 
-TEST_F(CPUTestFix, mov_register_indirect_with_displacement16_b_test)
+TEST_F(CPUTestFix, mov_register_indirect_with_displacement16_b_1)
 {
     this->h8->cpu.pc() = DummyMCU::area2_start;
     this->cpu->reg32(1).set(DummyMCU::ram_start);
@@ -22,4 +22,23 @@ TEST_F(CPUTestFix, mov_register_indirect_with_displacement16_b_test)
     EXPECT_EQ(ret, 0);
     int val = this->cpu->reg8(2).get();
     EXPECT_EQ(val, 0x56);
+}
+
+TEST_F(CPUTestFix, mov_register_indirect_with_displacement16_b_2)
+{
+    this->h8->cpu.pc() = DummyMCU::area2_start;
+    this->cpu->reg32(1).set(DummyMCU::ram_start);
+    this->cpu->reg8(2).set(0x78);
+
+    // mov.b r2,@(0x1234:16,er1)
+    this->dram->write8(0, 0x6e);
+    this->dram->write8(1, 0x92);
+    this->dram->write8(2, 0x12);
+    this->dram->write8(3, 0x34);
+
+    // 正しく命令が実行され、メモリに、レジスタ R2L から 0x78 がコピーされる
+    int ret = this->h8->execute_next_instruction();
+    EXPECT_EQ(ret, 0);
+    int val = this->mcu->read8(DummyMCU::ram_start + 0x1234);
+    EXPECT_EQ(val, 0x78);
 }
