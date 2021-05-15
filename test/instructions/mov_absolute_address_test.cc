cb2d375208fa46ddb39a70ac0aa6f2215b3db77e
SHIMOMURA Sho <graueule@gmail.com>
2021-05-15 14:24:30 +0900

    add test for mov absolute address w,l

--- test/instructions/mov_absolute_address_test.cc
+++ test/instructions/mov_absolute_address_test.cc
@@ -5,7 +5,7 @@
 // 命令の実行のテストと、命令のパースのテストは違う
 // ここで確認するのはパーサの選択やパースの実行結果ではなく、命令の実行結果
 
-TEST_F(CPUTestFix, mov_absolute_address_24_b_parse)
+TEST_F(CPUTestFix, mov_absolute_address_24_b)
 {
     this->h8->cpu.pc() = DummyMCU::area2_start;
     this->mcu->write8(DummyMCU::area2_start + 0x1000, 0x56);
@@ -25,3 +25,44 @@ TEST_F(CPUTestFix, mov_absolute_address_24_b_parse)
     EXPECT_EQ(val, 0x56);
 }
 
+TEST_F(CPUTestFix, mov_absolute_address_24_w)
+{
+    this->h8->cpu.pc() = DummyMCU::area2_start;
+    this->mcu->write16(DummyMCU::area2_start + 0x1000, 0x5678);
+
+    // RAM の開始アドレス(ram_start)は 0x400000
+    // mov.w @0x401000:24,r1
+    this->dram->write8(0, 0x6b);
+    this->dram->write8(1, 0x21);
+    this->dram->write8(2, 0x00);
+    this->dram->write8(3, 0x40);
+    this->dram->write8(4, 0x10);
+    this->dram->write8(5, 0x00);
+
+    int ret = this->h8->execute_next_instruction();
+    EXPECT_EQ(ret, 0);
+    int val = this->cpu->reg16(1).get();
+    EXPECT_EQ(val, 0x5678);
+}
+
+TEST_F(CPUTestFix, mov_absolute_address_24_l)
+{
+    this->h8->cpu.pc() = DummyMCU::area2_start;
+    this->mcu->write32(DummyMCU::area2_start + 0x1000, 0x567890ab);
+
+    // RAM の開始アドレス(ram_start)は 0x400000
+    // mov.l @0x401000:24,er1
+    this->dram->write8(0, 0x01);
+    this->dram->write8(1, 0x00);
+    this->dram->write8(2, 0x6b);
+    this->dram->write8(3, 0x21);
+    this->dram->write8(4, 0x00);
+    this->dram->write8(5, 0x40);
+    this->dram->write8(6, 0x10);
+    this->dram->write8(7, 0x00);
+
+    int ret = this->h8->execute_next_instruction();
+    EXPECT_EQ(ret, 0);
+    int val = this->cpu->reg32(1).get();
+    EXPECT_EQ(val, 0x567890ab);
+}
