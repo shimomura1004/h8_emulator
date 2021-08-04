997e0a4bb5be4205f6a5c170f0ade36a127595b2
SHIMOMURA Sho <graueule@gmail.com>
2021-08-04 21:49:05 +0900

    bugfix

--- test/instructions/extu_test.cc
+++ test/instructions/extu_test.cc
@@ -24,13 +24,13 @@ TEST_F(CPUTestFix, extu_l)
 {
     this->h8->cpu.pc() = DummyMCU::area2_start;
 
-    this->h8->cpu.reg16(1).set(0xf0f0f0f0);
+    this->h8->cpu.reg32(1).set(0xf0f0f0f0);
     this->dram->write8(0, 0x17);
     this->dram->write8(1, 0x71);
 
     int ret = this->h8->execute_next_instruction();
     EXPECT_EQ(ret, 0);
 
-    EXPECT_EQ(this->h8->cpu.reg16(1).get(), 0x0000f0f0);
+    EXPECT_EQ(this->h8->cpu.reg32(1).get(), 0x0000f0f0);
     EXPECT_EQ(this->h8->cpu.ccr().z(), false);
 }
